ba84e6092810898aa47d3a75e4c78ed6e9d6b564
Cristian Murarescu <cristian.murarescu@unity3d.com>
2026-05-22 14:30:57 +0100

    2 VAO & VBO and 2 frag shaders

--- src/main.cpp
+++ src/main.cpp
@@ -13,12 +13,21 @@ const char* vertexShaderSource =
 "}\n";
 
 // Fragment shader source code
-const char* fragmentShaderSource =
+const char* fragmentShaderSourceOne =
 "#version 330 core\n"
 "out vec4 FragColor;\n"
 "void main()\n"
 "{\n"
 "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n";
+
+// Fragment shader source code
+const char* fragmentShaderSourceTwo =
+"#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
 "}\n";
 
 // Function to process input
@@ -62,16 +71,16 @@ int main()
         return -1;
     }
 
-    float vertices[] = {
-        -0.5f, -0.5f, 0.0f,  // left  
-		 0.0f,  0.5f, 0.0f,  // top
-         0.5f, -0.5f, 0.0f,  // right 
-         
+	float verticesOne[] = {
+		-0.5f,  0.5f, 0.0f,  // top-left
+		 0.5f,  0.5f, 0.0f,  // top-right
+		 0.0f,  0.0f, 0.0f,  // tip (center)
+	};
 
-		 // second triangle
-		 0.5f, -0.5f, 0.0f, // left
-		 1.5f,  1.5f, 0.0f, // top
-		 1.5f, -0.5f, 0.0f, // right		 
+	float verticesTwo[] = {
+		-0.5f, -0.5f, 0.0f,  // bottom-left
+		 0.5f, -0.5f, 0.0f,  // bottom-right
+		 0.0f,  0.0f, 0.0f,  // tip (center)
 	};
 
 	//float vertices[] = {
@@ -88,32 +97,46 @@ int main()
 	//};
 
 	// Generate a vertex buffer object (VBO)
-    unsigned int vertexBufferObject;
-	glGenBuffers(1, &vertexBufferObject);
+    unsigned int vertexBufferObjects[2];
+	glGenBuffers(2, vertexBufferObjects);
 
 	// Generate a element buffer object (EBO)
 	//unsigned int elementBufferObject;
 	//glGenBuffers(1, &elementBufferObject);
 
 	// Generate a vertex array object (VAO)
-	unsigned int vertexArrayObject;
-	glGenVertexArrays(1, &vertexArrayObject);
+	unsigned int vertexArrayObjects[2];
+	glGenVertexArrays(2, vertexArrayObjects);
 
 	// Bind the vertex array object for rendering
-	glBindVertexArray(vertexArrayObject);
+	glBindVertexArray(vertexArrayObjects[0]);
 	
 	// Bind the vertex buffer object and copy the vertex data into it
-	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObjects[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verticesOne), verticesOne, GL_STATIC_DRAW);
+	
+	// Specify the layout of the vertex data
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
 
-	// Bind the element buffer object and copy the index data into it
-	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementBufferObject);
-	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glBindVertexArray(vertexArrayObjects[1]);
+
+	// Bind the vertex buffer object and copy the vertex data into it
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObjects[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verticesTwo), verticesTwo, GL_STATIC_DRAW);
 
 	// Specify the layout of the vertex data
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
+	// Bind the element buffer object and copy the index data into it
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementBufferObject);
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+		// Call after suspicious GL calls during setup
+	while (GLenum err = glGetError()) {
+		std::cerr << "GL error: 0x" << std::hex << err << std::endl;
+	}
 
 
     // Create a vertex shader object
@@ -137,38 +160,63 @@ int main()
 	}  
 
 	// Create a fragment shader object
-	unsigned int fragmentShader;
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	unsigned int fragmentShaderOne;
+	fragmentShaderOne = glCreateShader(GL_FRAGMENT_SHADER);
 
 	// Attach the shader source code to the fragment shader object and compile the shader
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
-	glCompileShader(fragmentShader);
+	glShaderSource(fragmentShaderOne, 1, &fragmentShaderSourceOne, nullptr);
+	glCompileShader(fragmentShaderOne);
 
 	// Check for fragment shader compile errors
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	glGetShaderiv(fragmentShaderOne, GL_COMPILE_STATUS, &success);
 	if (!success)
 	{
-		glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+		glGetShaderInfoLog(fragmentShaderOne, 512, nullptr, infoLog);
+		std::cerr << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	//Create a fragment shader object
+	unsigned int fragmentShaderTwo;
+	fragmentShaderTwo = glCreateShader(GL_FRAGMENT_SHADER);
+
+	// Attach the shader source code to the fragment shader object and compile the shader
+	glShaderSource(fragmentShaderTwo, 1, &fragmentShaderSourceTwo, nullptr);
+	glCompileShader(fragmentShaderTwo);
+
+	// Check for fragment shader compile errors
+	glGetShaderiv(fragmentShaderTwo, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShaderTwo, 512, nullptr, infoLog);
 		std::cerr << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
 	}
 
 	// Create a shader program object and link the vertex and fragment shaders to it
-	unsigned int shaderProgram;
-	shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
+	unsigned int shaderProgramOne;
+	shaderProgramOne = glCreateProgram();
+	glAttachShader(shaderProgramOne, vertexShader);
+	glAttachShader(shaderProgramOne, fragmentShaderOne);
+	glLinkProgram(shaderProgramOne);
 
 	// Check for shader program linking errors
-	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	glGetProgramiv(shaderProgramOne, GL_LINK_STATUS, &success);
 	if (!success) {
-		glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+		glGetProgramInfoLog(shaderProgramOne, 512, nullptr, infoLog);
 		std::cerr << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
 	}	
 
+	// Create a shader program object and link the vertex and fragment shaders to it
+	unsigned int shaderProgramTwo;
+	shaderProgramTwo = glCreateProgram();
+	glAttachShader(shaderProgramTwo, vertexShader);
+	glAttachShader(shaderProgramTwo, fragmentShaderTwo);
+	glLinkProgram(shaderProgramTwo);
+
+
 	// Delete the shader objects once we've linked them into the shader program
 	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
+	glDeleteShader(fragmentShaderOne);
+	glDeleteShader(fragmentShaderTwo);
 
 	// Create the viewport and set its dimensions (x, y, width, height)
 	glViewport(0, 0, 800, 600);
@@ -184,13 +232,21 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
 
 		// Use the shader program for rendering
-		glUseProgram(shaderProgram);
+		glUseProgram(shaderProgramOne);
 
 		// Bind the vertex array object for rendering
-		glBindVertexArray(vertexArrayObject);
+		glBindVertexArray(vertexArrayObjects[0]);
+		
 
 		// Draw the triangle using the currently bound vertex array object and shader program
-		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glUseProgram(shaderProgramTwo);
+
+		glBindVertexArray(vertexArrayObjects[1]);
+
+		// Draw the triangle using the currently bound vertex array object and shader program
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
